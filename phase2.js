65f54a8ac296c5a26fa099710a0220f411b9cacd
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2024-03-25 16:32:20 -0300

    Add dark mode
    include dark and light mode toggle button. Use absolute positioning to ensure button stays on screen when scrolling

--- phase2.js
+++ phase2.js
@@ -42,90 +42,21 @@ function saveToBrowser() {
 // Special note: the value of this variable is persistent
 // because it has global scope
 
-let evenIsDarkMode = 0; 
+let DarkMode = 1; 
 
 function toggleDarkMode() {
     // test whether evenIsDarkMode is an even or odd number
-    if (evenIsDarkMode % 2 == 0){
-        document.getElementById("main").classList.remove('dark');
-    } else {
-        document.getElementById("main").classList.add('dark');
+    if (DarkMode == 0){
+        document.getElementsByTagName("body")[0].classList.remove('dark');
+        DarkMode = 1;
+        console.log("DarkMode");
+      } else {
+        document.getElementsByTagName("body")[0].classList.add('dark');
+        DarkMode = 0;
+        console.log("LightMode");
     }
-    // increment evenIsDarkMode so its even or odd value is synchronized with
-    // the current display state
-    evenIsDarkMode++;
 }
 
-/**
- * Function for on click of "Submit button"
- */
-
-/**
- * Function for restoring save state
- */
-
-/**
- * Function for restoring submit state
- */
-
-/**
-function saveToBrowser() {
-    if (typeof Storage !== "undefined") {
-      const nameQuote = {
-        name: document.getElementById("nameInput").value,
-        quote: document.getElementById("quoteInput").value,
-      };
-  
-      window.localStorage.setItem("nameQuoteObject", JSON.stringify(nameQuote));
-  
-      document.getElementById("nameInput").value = "";
-      document.getElementById("quoteInput").value = "";
-    } else {
-      console.log("Browser storage unavailable!");
-    }
-  }
-  
-
-  function popOutOfBrowser() {
-    if (typeof Storage !== "undefined") {
-      const object = JSON.parse(window.localStorage.getItem("nameQuoteObject"));
-  
-      const name = object.name;
-  
-      const quote = object.quote;
-  
-      const concatString = name + ', "' + quote + '"';
-  
-      document.getElementById("popOutText").innerText = concatString;
-    } else {
-      console.log("Browser storage unavailable!");
-    }
-  }
-  
-
-if (typeof Storage !== "undefined") {
-  
-    const naturalClient = JSON.parse(window.localStorage.getItem("myKey"));
-    
-    for (let i = 0; i < naturalClient.clientInfo.length; i++){
-      console.log(naturalClient.clientInfo[i]);
-    }
-    
-  } else {
-    console.log("Local storage is not available.");
-  }
-
-
-const naturalClient = {clientInfo:[{name:"Fred Goodlife",birthdate:1955},{name:"George Young",birthdate:1980},
-{name:null,birthdate:null},{name:null,birthdate:null},{name:null,birthdate:null}]};
-
-
-if (typeof Storage !== "undefined") {
-
-  window.localStorage.setItem("myKey", JSON.stringify(naturalClient));
-
-} else {
-  console.log("Local storage is not available.");
+tailwind.config = {
+  darkMode: 'selector',
 }
-
-*/
