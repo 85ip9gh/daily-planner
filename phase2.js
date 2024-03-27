2d91be4939ca19d22bc84ab8740d95fe2afb5b0a
Ironhood6670 <zacharyivanoff@gmail.com>
2024-03-27 16:20:38 -0300

    Added auto load of saved data from previous session

--- phase2.js
+++ phase2.js
@@ -9,27 +9,52 @@
  * Function for on click of "Save button"
  */
 function saveToBrowser() {
-    //initalize an empty JS object
-    const formObject = {};
+  //initalize an empty JS object
+  const formObject = {};
 
-    //loop through a structure holding keys (html elements), and store value of html elements
-    const inputs = document.querySelectorAll('input');
-    
-    inputs.forEach(function(input) {
-        if(input.type == 'radio') {
-          if (input.checked) {
-          console.log("radio key: " + input.name + " value: " + input.value);
-          formObject[input.name] = input.value;
-          }
-        } else {
-          console.log("key: " + input.name + " value: " + input.value);
-          formObject[input.name] = input.value;
-        }
-    });
+  //loop through a structure holding keys (html elements), and store value of html elements
+  const inputs = document.querySelectorAll('input');
 
-    //store the whole object into local browser storage (permanent)
-    localStorage.setItem('formData', JSON.stringify(formObject));
+  inputs.forEach(function (input) {
+    if (input.type == 'radio') {
+      if (input.checked) {
+        console.log("radio key: " + input.name + " value: " + input.value);
+        formObject[input.name] = input.value;
+      }
+    } else {
+      console.log("key: " + input.name + " value: " + input.value);
+      formObject[input.name] = input.value;
+    }
+  });
+
+  //store the whole object into local browser storage (permanent)
+  localStorage.setItem('formData', JSON.stringify(formObject));
 }
+
+/**
+ * Function for on load, pulls any previously saved data from local browser storage
+ */
+function getFromBrowser() {
+  const retrievedObject = JSON.parse(window.localStorage.getItem('formData'));
+
+  const inputs = document.querySelectorAll('input');
+
+  const elements = Object.values(retrievedObject);
+
+  let count = 0;
+  for (var i in inputs) {
+    if (inputs[i].type == "radio") {
+      if (inputs[i].value == elements[count]) {
+        inputs[i].checked = true;
+        count++;
+      }
+    } else {
+      inputs[i].value = elements[count];
+      count++;
+    }
+  }
+}
+
 
 /* 
    The purpose of this file is to define the function to toggle dark mode.
@@ -42,17 +67,17 @@ function saveToBrowser() {
 // Special note: the value of this variable is persistent
 // because it has global scope
 
-let DarkMode = 1; 
+let DarkMode = 1;
 
 function toggleDarkMode() {
-    // test whether evenIsDarkMode is an even or odd number
-    if (DarkMode == 0){
-        document.getElementsByTagName("body")[0].classList.remove('dark');
-        DarkMode = 1;
-        document.getElementsByTagName("button")[0].innerHTML = "Dark Mode";
-      } else {
-        document.getElementsByTagName("body")[0].classList.add('dark');
-        DarkMode = 0;
-        document.getElementsByTagName("button")[0].innerHTML = "Light Mode";
-    }
+  // test whether evenIsDarkMode is an even or odd number
+  if (DarkMode == 0) {
+    document.getElementsByTagName("body")[0].classList.remove('dark');
+    DarkMode = 1;
+    document.getElementsByTagName("button")[0].innerHTML = "Dark Mode";
+  } else {
+    document.getElementsByTagName("body")[0].classList.add('dark');
+    DarkMode = 0;
+    document.getElementsByTagName("button")[0].innerHTML = "Light Mode";
+  }
 }
