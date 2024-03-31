b0c9f9803daa680bd095e2bcf4914cb185d80d47
dan <damirault3@gmail.com>
2024-03-31 16:20:26 -0300

    Add documentation to the JS file.
    Documentation is in compliance with the CSCI2356 doc standard.

--- phase2.js
+++ phase2.js
@@ -1,12 +1,14 @@
 /**
- * Purpose: Holds functions and code for storing all user information
- * in a single JSON object into local browser storage, and restoring.
+ * Purpose: JS file to hold functions to handle saving the form to
+ * browser storage, loading it from browser storage, toggling dark mode
+ * and playing audio
  *
- * Author: Daniel Amirault
+ * Author(s): Daniel Amirault, Pesanth Janaseth Rangaswamy Anitha, Zachary Ivanoff, Susan Macinnis 
  */
 
 /**
- * Function for on click of "Save button"
+ * Function to save the form's fields, in a single object in browser storage
+ * on click of save button.
  */
 function saveToBrowser() {
   //initalize an empty JS object
@@ -16,6 +18,7 @@ function saveToBrowser() {
   const inputs = document.querySelectorAll('input');
 
   inputs.forEach(function (input) {
+    //if a field is a radio button, only save if checked
     if (input.type == 'radio') {
       if (input.checked) {
         console.log("radio key: " + input.name + " value: " + input.value);
@@ -33,42 +36,46 @@ function saveToBrowser() {
 
 /**
  * Function for on load, pulls any previously saved data from local browser storage
+ * into the form's fields.
  */
 function getFromBrowser() {
+  //parse the object from browser storage
   const retrievedObject = JSON.parse(window.localStorage.getItem('formData'));
 
+  //select all the input fields
   const inputs = document.querySelectorAll('input');
 
+  //get the values of the parsed JSON object
   const elements = Object.values(retrievedObject);
 
+  //initialize a count variable for radio buttons having multiple fields
   let count = 0;
+
+  //loop through the input fields
   for (var i in inputs) {
+    //if a field is a radio button
     if (inputs[i].type == "radio") {
+      //check if radio button option matches the one saved into the JSON object
       if (inputs[i].value == elements[count]) {
+        //if so, check the radio button
         inputs[i].checked = true;
         count++;
       }
     } else {
+      //if not a radio button, simply place the values of the JSON object into the input's value
       inputs[i].value = elements[count];
       count++;
     }
   }
 }
 
-
-/* 
-   The purpose of this file is to define the function to toggle dark mode.
-
-   Author: Terry
-*/
-
-// used to indicate whether the current display state
-// is dark mode (even) or light mode (odd)
-// Special note: the value of this variable is persistent
-// because it has global scope
-
+//Global variable in order to have behaviour be persistent
 let DarkMode = 1;
 
+/**
+ * Function used to indicate whether the current display state is dark mode (even) or light mode (odd)
+ * This has been adapted from Terry's solution (More tailwind > tailToggleFunc) for our website
+ */
 function toggleDarkMode() {
   // test whether evenIsDarkMode is an even or odd number
   if (DarkMode == 0) {
@@ -83,6 +90,10 @@ function toggleDarkMode() {
 }
 
 
+/**
+ * Function for playing the audio element nested within an html element,
+ * on click of the Gold button.
+ */
 function playAudio(element) {
   var audio = element.querySelector('audio');
   audio.play();
